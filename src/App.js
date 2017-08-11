d3783c7848cb29ae41734798bd07cabc59c24aaa
Kimak <mickael.dumand@gmail.com>
2017-08-11 09:27:17 +0200

    start header landing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from './theme';
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
+import homeBackground from './assets/home-bg-repeat.png';
 
 const Root = styled.div``;
-/* background-color: ${props => props.theme.colors.blackcurrant}; */
-/* background-image: url(${background}); */
+
 const Header = styled.div`
   text-align: center;
+  position: relative;
+  height: 600px;
+  background-repeat: repeat;
+  background-image: url(${homeBackground});
+  background-position: -70px -70px;
+  overflow: hidden;
+`;
+
+const HeaderTitle = styled.h2`
+  margin: 10px 0 0 0;
+  font-weight: normal;
+  font-size: 25px;
   color: ${props => props.theme.colors.dandelion};
+`;
+
+const HeaderSubTitle = styled.h3`
+  margin: 0;
+  font-weight: normal;
+  font-size: 16px;
+  letter-spacing: 1.5px;
+  color: ${props => props.theme.colors.white};
+`;
+
+const HeaderLogo = styled.div`
+  position: relative;
+  top: 157px;
+`;
+
+const HeaderBg = styled.div`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateY(-50%) translateX(-50%);
+  height: 200px;
+  width: 200%;
+  left: -50%;
+  bottom: -15%;
+  transform: rotateZ(-7deg);
+  background-color: ${props => props.theme.colors.blackcurrant};
 `;
 
 function App() {
@@ -20,8 +51,15 @@ function App() {
     <ThemeProvider theme={theme}>
       <Root>
         <Header>
-          <img src={logo} alt="logo" />
-          <h2>Clever Monkey</h2>
+          <HeaderLogo>
+            <img src={logo} alt="logo" />
+            <HeaderTitle>Clever Monkey</HeaderTitle>
+            <HeaderSubTitle>
+              {/* Freelance Software Developer */}
+              Expert ReactNative
+            </HeaderSubTitle>
+          </HeaderLogo>
+          <HeaderBg />
         </Header>
       </Root>
     </ThemeProvider>
